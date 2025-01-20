f4bdd81ff6e2d65e4392cbf97959b6dbfbddd089
titohajili <maqatoto963@gmail.com>
2025-01-20 23:43:31 +0400

    Navigation bar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
