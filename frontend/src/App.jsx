9f7fb7a8aa1d7d337543d52f9acf6411f25cfe24
titohajili <maqatoto963@gmail.com>
2025-01-21 21:49:46 +0400

    Navigation bar finish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <NavBar/>
-      <Routes>
+      <Routes> 
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
         <Route path='/about' element={<About />} />
