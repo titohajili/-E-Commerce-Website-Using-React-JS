5577dfb35c4b9e0174df68361b98d165448b97cb
titohajili <maqatoto963@gmail.com>
2025-01-27 23:32:53 +0400

    Latest Porduct section

--- frontend/src/components/LatesCollection.jsx
+++ frontend/src/components/LatesCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
+import ProductItem from './ProductItem'
 
 const LatesCollection = () => {
 
@@ -20,6 +21,15 @@ const LatesCollection = () => {
         </p>
       </div>
 
+    {/* {Rendering Products} */}
+
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+      {
+        latestProducts.map((item,index)=>(
+          <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+        ))
+      }
+    </div>
 
     </div>
   )
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
-        <img src={assets.logo} className='w-36' alt="" />
+       <Link to={'/'}> <img src={assets.logo} className='w-36' alt="" /> </Link>
       
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
