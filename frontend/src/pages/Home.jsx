efbf7cbaefdfa26e76b051f88e359bcc5cf9dee3
titohajili <maqatoto963@gmail.com>
2025-01-28 23:34:46 +0400

    Best Seller Product

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatesCollection from '../components/LatesCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatesCollection />
+      <BestSeller />
     </div>
   )
 }
