07b84c77f687a02337276254fbb32ca6dc6c6372
titohajili <maqatoto963@gmail.com>
2025-01-30 21:38:55 +0400

    Policies and Newsletter box

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatesCollection from '../components/LatesCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatesCollection />
       <BestSeller />
+      <OurPolicy/>
+      <NewsletterBox/>
     </div>
   )
 }
