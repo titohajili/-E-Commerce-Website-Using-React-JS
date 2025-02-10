2f8e4291c5a14bebd6ce0976e7ff5b500c3c030d
titohajili <maqatoto963@gmail.com>
2025-02-10 15:17:24 +0400

    Product Page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
-    <div>
-      
+
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  condt [Image, setImage] = useState('');
+
+  const fetchProductData = async () =>{
+    
+    products.map((item)=>{
+      if(item._id === productId){
+        setProductData(item)
+        setImage(item.image[0])
+        return null;
+      }
+    })
+
+  }
+  
+  useEffect(()=>{
+    fetchProductData();
+  }, [productId])
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opasity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+          {/* Product Image */}
+          <div className='flex-1 felex flex-col-reverse gap-3 sm:flex-row'>
+            <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+                
+            </div>
+          </div>
+      </div>
+
     </div>
-  )
+  ) : <div className='opasity-0'></div>
 }
 
 export default Product
