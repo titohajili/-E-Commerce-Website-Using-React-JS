f05629c8b2b767f843c43461cf8191c10d14ff7b
titohajili <maqatoto963@gmail.com>
2025-03-05 23:37:19 +0400

    Place Order Page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/frontend_assets/assets'
+import { useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const PlaceOrder = () => {
+
+  const [method, setMethod] = useState('cod');
+  const {navigate} = useContext(ShopContext)
   return (
     <div className='flex felx-col sm:-row justify-between gap-4 pt-5 sm:pt-14 min-h-[8vh] border-t'>
       {/* left side */}
@@ -43,8 +49,26 @@ const PlaceOrder = () => {
 
         <div className='mt-12'>
             <Title text1={'PAYMENT'}  text2={'METHOD'} />
-            <div className='flex gap-3 flex-col flex-row'>
+            {/* payment methods selection */}
+            <div className='flex gap-3 flex-col lg:flex-row'>
+                <div onClick={()=>setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                  <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
+                  <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
+                </div>
 
+                <div onClick={()=>setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                  <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
+                  <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
+                </div>
+
+                <div onClick={()=>setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                  <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
+                  <p className='text-gray-500  text-sm font-medium mx-4'>Cash on Delivery</p>
+                </div>
+            </div>
+
+            <div className='w-full text-end mt-8'>
+              <button onClick={()=>navigate('/orders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
             </div>
         </div>
 
